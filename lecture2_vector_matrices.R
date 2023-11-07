87de64646072edd5340fd12092e6ee30f2a8b0d8
thomasSierocinski <100637992+thomasSierocinski@users.noreply.github.com>
2023-11-07 11:56:12 -0800

    Add files via upload

--- lecture2_vector_matrices.R
+++ lecture2_vector_matrices.R
@@ -1,280 +1,313 @@
-# vectors
-
-# inititialize
-
-v <- c(2,3,4)
-class(v)
-
-# various ways to generate data
-v <- 2:4
-v
-
-v <- seq(2, 4, by=1)
-v
-
-v <- rep(v, times=3)
-v
-
-v <- rep(v, each=3)
-v
-
-# generating data distribution
-u <- runif(100, min=1, max=10)
-n <- rnorm(100)
-r <- rpois(100, lambda=1)
-sample(10, size=100, replace=TRUE)
-sample(u, size=10)
-
-#plotting distributions
-hist(u)
-hist(n)
-hist(r)
-
-plot(density(u), ylim=c(0, max(density(r)$y)), main="data density", xlab="data", ylab="density")
-lines(density(n), col="red")
-lines(density(r), col="blue")
-legend(11, 0.7, legend=c("u", "n", "r"),  fill=c("black", "red", "blue"))
-
-
-# exporting figure
-dev.copy(png, "dataDensity1.png")
-dev.off()
-
-png("dataDensity2.png")
-plot(density(u), ylim=c(0, max(density(r)$y)), main="data density", xlab="data", ylab="density")
-lines(density(n), col="red")
-lines(density(r), col="blue")
-legend(11, 0.7, legend=c("u", "n", "r"),  fill=c("black", "red", "blue"))
-dev.off()
-
-# testing equality
-a <- 1
-b <- 1
-c <- 2
-a==b
-a==c
-
-v1 <- c(2,3,4)
-v2 <- c(2,3,4)
-v3 <- c(2,3,5)
-v1==v2
-v1==v3
-which(v1==v3)
-which(v1!=v3)
-all(v1==v2)
-all(v1==v3)
-
+### VECTORS
 # vector opposite
 v1 <- c(2,3)
-v2 <- -v1
-plot(NULL, xlim=c(-5,5), ylim=c(-5,5), xlab="X", ylab="Y")
-arrows(x0=0, y0=0, x1=v1[1], y1=v1[2], col="red")
-arrows(x0=0, y0=0, x1=v2[1], y1=v2[2], col="blue")
-# parallel vector
-v3 <- v1+1
-arrows(x0=1, y0=1, x1=v3[1], y1=v3[2], col="green")
-v3 <- v1 + c(0,1)
-arrows(x0=0, y0=1, x1=v3[1], y1=v3[2], col="green")
-
-# vector addition
-v1 <- c(2,3)
+-v1
+# parallel
+v1+1
+# addition
 v2 <- rev(v1)
-v3 <- v1+v2
-plot(NULL, xlim=c(-5,5), ylim=c(-5,5), xlab="X", ylab="Y")
-arrows(x0=0, y0=0, x1=v1[1], y1=v1[2], col="red")
-arrows(x0=0, y0=0, x1=v2[1], y1=v2[2], col="green")
-arrows(x0=0, y0=0, x1=v3[1], y1=v3[2], col="orange")
-arrows(x0=v1[1], y0=v1[2], x1=v3[1], y1=v3[2], col="red", lty=2)
-arrows(x0=v2[1], y0=v2[2], x1=v3[1], y1=v3[2], col="green", lty=2)
-
-# vector substraction
-v1 <- c(2,3)
-v2 <- rev(v1)
-v3 <- v1-v2
-plot(NULL, xlim=c(-5,5), ylim=c(-5,5), xlab="X", ylab="Y")
-arrows(x0=0, y0=0, x1=v1[1], y1=v1[2], col="red")
-arrows(x0=0, y0=0, x1=v2[1], y1=v2[2], col="green")
-arrows(x0=0, y0=0, x1=v3[1], y1=v3[2], col="orange", lty=2)
-arrows(x0=v2[1], y0=v2[2], x1=v1[1], y1=v1[2], col="orange")
-
+v1+v2
+# substraction
+v1-v2
 # scalar product
-v1 <- c(2,3)
-v2 <- 2*v1
+2*v1
+## norm
+sqrt((v1[1]^2+v1[2]^2))
+norm(c(2,3), type="2")
+# dot product
+v1 %*% v2
+# projection
+v2 <- c(3,0)
+
+pv1v2 <- as.vector((v1 %*% v2)/(v2 %*% v2)) * v2
 plot(NULL, xlim=c(-6,6), ylim=c(-6,6), xlab="X", ylab="Y")
 arrows(x0=0, y0=0, x1=v1[1], y1=v1[2], col="red")
 arrows(x0=0, y0=0, x1=v2[1], y1=v2[2], col="green")
+arrows(x0=0, y0=0, x1=pv1v2[1], y1=pv1v2[2], col="orange")
 
 
-# vector multiplication
-# not a cross product
-v1 <- c(2,3)
-v2 <- c(1,2)
-v3 <- v1*v2
+
+
+
+
+#### VECTOR SPACE
+
+u <- c(2,3,4)
+v <- c(3,5,7)
+w <- c(6,3,1)
+# Associativity of vector addition
+all(u+(v+w)==(u+v)+w)
+# Commutativity of vector addition
+all(u+v==v+u)
+#Identity element of vector addition
+all(v+0==v)
+#Inverse elements of vector addition
+v+(-v)
+#Compatibility of scalar multiplication with field multiplication
+a=3
+b=8
+all(a*(b*v)==(a*b)*v)
+#Identity element of scalar multiplication
+all(1*v==v)
+#Distributivity of scalar multiplication with respect to vector addition
+all(a*(u+v)==a*u+a*v)
+#Distributivity of scalar multiplication with respect to field addition
+all((a+b)*v==a*v+b*v)
+
+
+
+### LINEAR MAPS
+
+f <- function(v){
+  result <- NA
+  if (length(v==2)){
+    result <- c(2*v[1], v[2])
+  }
+  else{
+    print("length(v) must equal 2")
+  }
+  return(result)
+}
+
+a <- c(-3,2)
+b <- c(2,2)
+f(a)
+f(b)
+all(f(a+b)==(f(a)+f(b)))
+all(6*f(a)==f(6*a))
+
+linMap <- function(v, m){
+  result <- NA
+  res <- m %*% v
+  return(res)
+}
+vlist=vector(mode = "list", length = 10)
+vlist[[1]]<-a
+# rotation
+# 90 degrees counterclockwise
+vlist[[2]] <- linMap(a, matrix(c(0,-1,1,0), nrow=2, ncol=2, byrow=TRUE))
+# using d degree angle counterclockwise
+d=pi / 3
+vlist[[3]] <- linMap(a, matrix(c(cos(d),-sin(d),sin(d),cos(d)), nrow=2, ncol=2, byrow=TRUE))
+# reflection
+# x axis
+vlist[[4]] <- linMap(a, matrix(c(1,0,0,-1), nrow=2, ncol=2, byrow=TRUE))
+# y axis
+vlist[[5]] <- linMap(a, matrix(c(-1,0,0,1), nrow=2, ncol=2, byrow=TRUE))
+# line of d degrees with x axis
+vlist[[6]] <- linMap(a, matrix(c(cos(2*d),sin(2*d),sin(2*d),-cos(2*d)), nrow=2, ncol=2, byrow=TRUE))
+# scaling
+s=2
+vlist[[7]] <- linMap(a, matrix(c(s,0,0,s), nrow=2, ncol=2, byrow=TRUE))
+# horizontal sheer
+m=2.5
+vlist[[8]] <- linMap(a, matrix(c(1,m,0,1), nrow=2, ncol=2, byrow=TRUE))
+# squeeze
+k=2
+vlist[[9]] <- linMap(a, matrix(c(k,0,0,1/k), nrow=2, ncol=2, byrow=TRUE))
+# projection onto the y axis
+vlist[[10]] <- linMap(a, matrix(c(0,0,0,1), nrow=2, ncol=2, byrow=TRUE))
+
+
+library(RColorBrewer)
+labls <- c("a", "counterclockwise", "30 degrees", "refl x", "refl y", "refl 30 d", "scaling", "sheer", "squeeze", "projection")
+col <- brewer.pal(n = 10, name = "Paired")
 plot(NULL, xlim=c(-6,6), ylim=c(-6,6), xlab="X", ylab="Y")
-arrows(x0=0, y0=0, x1=v1[1], y1=v1[2], col="red")
-arrows(x0=0, y0=0, x1=v2[1], y1=v2[2], col="green")
-arrows(x0=0, y0=0, x1=v3[1], y1=v3[2], col="orange")
-
-
-#ordering vectors
-
-
-# possible excercise
-# test axioms of vector space
-# generate 3 random vectors of identical length N
+sapply(1:10, function(i,v=vlist,c=col,l=labls){
+  arrows(x0=0, y0=0, x1=v[[i]][1], y1=v[[i]][2], col=c[i])
+  text(v[[i]][1], v[[i]][2], l[i], col=c[i])
+}, vlist, col, labls)
+abline(a=0, b=0.3)
+dev.off()
 
 
 
+### LINEAR EQUATION
+
+rm(list=ls())
+
+m <- matrix(c(2,3,5,-1), nrow=2, ncol=2, byrow = TRUE)
+o <- c(8,-2)
+
+v <- solve(m, o)
+v
+
+m %*% v 
 
 
-# matrices
-# matrix creation
-mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
-               dimnames = list(c("row1", "row2"),
-                               c("C.1", "C.2", "C.3")))
-mdat
-
-# dimension
-dim(mdat)
-length(mdat[1,])
-length(mdat[,2])
 
 
-# appending matrices
-mdatApp1 <- cbind(mdat, mdat)
-mdatApp2 <- rbind(mdat, mdat)
+### EIGEN DECOMPOSITION
 
-mdat <- matrix(nrow = 100, ncol = 10)
-  # for loop
-  for(i in 1:length(mdat[,1])){
-    mdat[i,] <- runif(10)
-  }  
+rm(list=ls())
+m <- matrix(c(-5,2,2,4), nrow=2, ncol=2, byrow = TRUE)
+m
 
+e <- eigen(m)
+e
+
+# verification
+# first eigenvector
+m %*% e$vectors[,1]
+e$values[1]*e$vectors[,1]
+# second eigenvector
+m %*% e$vectors[,2]
+e$values[2]*e$vectors[,2]
+
+
+
+### VECTOR SPACE MODEL
+
+rm(list=ls())
+
+m <- matrix(c(4,1,1,3), nrow=2, ncol=2, byrow = TRUE)
+row.names(m) <- c("d1", "d2")
+colnames(m) <- c("T1", "T2")
+m
+
+
+cosineDist <- function(v1, v2){
+  return(sum(v1*v2)/(sqrt(sum(v1^2))*sqrt(sum(v2^2))))
+}
+
+
+query <- c(1,1)
+
+similarity <- apply(m, 1, function(x, q=query){
+  cosineDist(x, q)
+}, query)
+
+sort(similarity, decreasing = TRUE)
+
+plot(NULL, xlim=c(-6,6), ylim=c(-6,6), xlab="T1", ylab="T2")
+arrows(x0=0, y0=0, x1=m[1,1], y1=m[1,2], col="blue")
+text(m[1,1], m[1,2], "d1", col="blue")
+arrows(x0=0, y0=0, x1=m[2,1], y1=m[2,2], col="green")
+text(m[2,1], m[2,2], "d2", col="green")
+arrows(x0=0, y0=0, x1=query[1], y1=query[2], col="orange")
+text(query[1], query[2], "query", col="orange")
+
+
+
+
+### DISTANCES
+
+rm(list=ls())
 # functions
 munif <- function(x, y){
   print(x)
   print(y)
   mres <- matrix(nrow=x, ncol=y)
   for(i in 1:length(mres[,1])){
-    mres[i,] <- runif(10)
+    mres[i,] <- runif(y)
   }
   return(mres)
 }
 
-mdat <- munif(100, 10)
-
-
-
-# apply
-mdat2 <- apply(mdat, 1, function(x){x*runif(1)})
-#same than
-mdat3 <- mdat*runif(100)
-
-
-
-# sapply
-mdat <- sapply(1:100, function(x){runif(10)})
-mdat <- t(mdat)
-
-#lapply
-mdat <- lapply(1:100, function(x){runif(10)})
-class(mdat)
-mdat2 <- do.call(rbind, mdat)
-
-
-
-# subset matrix
-mdat <- matrix(nrow = 100, ncol = 10)
+mdat <- munif(10, 5)
+#euclidean
+dist(mdat, method = "euclidean")
+# manhattan
+dist(mdat, method = "manhattan")
+# cosine
 # for loop
-for(i in 1:length(mdat[,1])){
-  mdat[i,] <- runif(10)
+dm <- matrix(nrow=length(mdat[,1]), ncol=length(mdat[,1]))
+
+
+for (i in 1:(length(mdat[,1]))){
+  for (j in (i):length(mdat[,1])){
+    #print(paste("i=", i , ", j=", j, sep=""))
+    dm[i,j] <- cor(mdat[i,], mdat[j,], method = "pearson")
+    dm[j,i] <- dm[i,j]
+  }
+  dm[i,i]<-1
 }
 
-idx<-10
-mdat[idx,]
-mdat[,idx]
-mdat[idx,idx]
-
-mdat[10:20,]
-mdat[,1:5]
-mdat[10.20,1:5]
-
-
-# transpose matrix
-mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
-          dimnames = list(c("row1", "row2"),
-                          c("C.1", "C.2", "C.3")))
-mdat
-t(mdat)
-
-# reorder matrix
-colOrder <- c(3,2,1)
-rowOrder <- c(2,1)
-mdat
-mdat[,colOrder]
-mdat[rowOrder,]
-mdat[rowOrdfer, ColOrder]
+# sapply
+dms <- sapply(seq(1:length(mdat[,1])), function(i, m=mdat){
+  sapply(seq(1:length(mdat[,1])), function(j, mm=m){
+    #print(paste("i=", i, ", j=", j, sep=""))
+    return(cor(mm[i,], mm[j,], method="pearson"))
+  },m)
+}, mdat)
 
 
 
 
-# matrix addition
-mdat1 <- munif(100, 10)
-mdat2 <- munif(100, 10)
-mdat1+mdat2
-mdat3 <- munif(101, 10)
-mdat1 + mdat3
+### DISTANCE MATRIX
+
+rm(list=ls())
+#install.packages(c("Hmisc", "corrplot"))
+library("Hmisc")
+library("corrplot")
+
+D1 <- "Text mining is to find useful information from text"
+D2 <- "Useful information is mined from text"
+D3 <- "dark came"
+D4 <- "mining in the dark is hard"
+D5 <- "a dark text on mining"
+
+H <- tolower(c(D1, D2, D3, D4, D5))
+H <- unlist(strsplit(H, " "))
+H <- H[!duplicated(H)]
+H <- H[-which(H %in% c("a", "is", "to", "the", "in", "from", "on"))]
+
+TD <- matrix(nrow=5, ncol=length(H))
+TD[1,] <- c(1,1,1,1,1,0,0,0,0)
+TD[2,] <- c(1,0,0,1,1,1,0,0,0)
+TD[3,] <- c(0,0,0,0,0,0,1,1,0)
+TD[4,] <- c(0,1,0,0,0,0,1,0,1)
+TD[5,] <- c(1,1,0,0,0,0,1,0,0)
+colnames(TD) <- H
+row.names(TD) <- c("D1", "D2", "D3", "D4", "D5")
+
+mydata.rcorr = rcorr(as.matrix(t(TD)))
+mydata.coeff = mydata.rcorr$r
+mydata.p = mydata.rcorr$P
+corrplot(mydata.coeff)
+#corrplot(mydata.coeff, p.mat=mydata.p, sig.level = 0.5)
 
 
-# matrix multiplications
-# by scalar
-mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
-              dimnames = list(c("row1", "row2"),
-                              c("C.1", "C.2", "C.3")))
-mdat
-mdat2 <- 2*mdat
+### HOW MUCH RAM DO I NEED?
 
-# by vector
-rowVec <- c(1,2,3)
-colVec <- c(4,5)
-mdat*rowVec
-mdat*colVec
+howMuchRam <- function(l, c){
+  return(l*c*8/(10^9))
+}
 
-mdat <- matrix(c(1,2, 11,12), nrow = 2, ncol = 2, byrow = TRUE,
-               dimnames = list(c("row1", "row2"),
-                               c("C.1", "C.2")))
-mdat
-rowVec <- c(1,2)
-colVec <- c(4,5)
-mdat*rowVec
-mdat*colVec
+v <- c(1, 10, 100, 1000, 10000, 50000, 100000, 250000, 500000, 750000, 1000000)
+res <- matrix(nrow=length(v), ncol=length(v))
+
+for (i in 1:length(v)){
+  for (j in 1:length(v)){
+    res[i, j] <- howMuchRam(v[i], v[j])
+  }
+}
+
+row.names(res) <- v
+colnames(res) <- v
+res
 
 
-# mutliplication
-mdat
-mdat * mdat
+### PARALLELIZATION AND TIMING
+
+rm(list=ls())
+library(parallel)
+
+munif <- function(x, y){
+  mres <- matrix(nrow=x, ncol=y)
+  for(i in 1:length(mres[,1])){
+    mres[i,] <- runif(10)
+  }
+  return(mres)
+}
+mdat <- munif(1000000, 10)
+
+system.time(test1 <- apply(mdat, 1, mean))
+
+no_cores <- detectCores()
+clust <- makeCluster(no_cores)
+system.time(test2 <- parApply(clust, mdat, function(x){mean(x)}))
+stopCluster(clust)
 
 
-# %*% operator
-mdat
-mdat %*% mdat
 
-
-# matrix types
-# diagonal matrix
-d <- diag(3)
-d
-
-# triangular
-m2 <- matrix(1:20, 4, 5)
-lower.tri(m2)
-m2[lower.tri(m2)] <- NA
-m2
-
-# symmetric
-s<-matrix(1:25,5)
-s[lower.tri(s)] <- t(s)[lower.tri(s)]
-
-# inverse
-s1 <- solve(s)
-print(s1)
