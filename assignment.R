d64e9f21db8100b3c5463789f2aafaf96a44a96f
thomasSierocinski <100637992+thomasSierocinski@users.noreply.github.com>
2024-12-10 14:30:14 -0800

    Add files via upload

---
+++ assignment.R
@@ -0,0 +1,13 @@
+# Select dataset
+
+# briefly describe dataset/study 
+
+# select an endpoint of interest for analysis below (tumor vs normal for instance)
+
+# Perform data normalization and missing data imputation (if required) 
+
+# Perform feature selection (plot p:value distribution) on your endpoint using your choice of method and threshold, briefly explain your choices
+
+# Using selected features, perform any type of clustering on samples, briefly comment on the approach and results
+
+# using selected features, perform one type of dimensionality reduction of your choice/ briefly comment on your choices and the results obtained
